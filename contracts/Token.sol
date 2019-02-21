3d65fd81a3e01f6b84d7edc046620c60ddba5f54
Juncheng <frankdfrayoud34@163.com>
2019-02-21 17:51:00 +0800

    Update Token.sol (#25)
    * Update Token.sol
    
    Update Token.sol for latest compiler
    
    * Update Token.sol

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,32 +1,31 @@
-pragma solidity ^0.4.16;
+pragma solidity >=0.4.0 <0.6.0;
 
 contract Token {
     address issuer;
-    mapping (address => uint) balances;
+    mapping (address => uint) public balances;
 
     event Issue(address account, uint amount);
     event Transfer(address from, address to, uint amount);
 
-    function Token() {
+    constructor () public {
         issuer = msg.sender;
     }
 
-    function issue(address account, uint amount) {
+    function issue(address account, uint amount) public {
         require(msg.sender == issuer);
         balances[account] += amount;
     }
 
-    function transfer(address to, uint amount) {
+    function transfer(address to, uint amount) public {
         require(balances[msg.sender] >= amount);
 
         balances[msg.sender] -= amount;
         balances[to] += amount;
 
-        Transfer(msg.sender, to, amount);
+        emit Transfer(msg.sender, to, amount);
     }
 
-    function getBalance(address account) constant returns (uint) {
+    function getBalance(address account) public view returns (uint) {
         return balances[account];
     }
 }
-
